32cb89c1272f75c18e76d6b2865f5bf24a98f489
yao qiu <yuri1100@qq.com>
2025-12-22 16:41:28 +0800

    1. T5 model use origin format, because offload_model arg will empty its weight mem usage, there is no need to use nvfp4 2. add sageattn arg and torch mem segment arg in shell script

--- run_w4a16.sh
+++ run_w4a16.sh
@@ -1,3 +1,5 @@
+export USE_SAGEATTN=1
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 python3 generate_w4a16.py  --task t2v-A14B \
   --size 1280*720 \
   --quantized_ckpt_dir /data/models/wan/wan2.2_T2V_14B_NVFP4 \
@@ -5,4 +7,5 @@ python3 generate_w4a16.py  --task t2v-A14B \
   --offload_model True \
   --convert_model_dtype \
   --enable_hooks \
+  --frame_num 120 \
   --prompt "Two anthropomorphic cats in comfy boxing gear and bright gloves fight intensely on a spotlighted stage."
