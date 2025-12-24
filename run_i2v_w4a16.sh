8cf5a65005b968e0013112a17a5a4d9810c9c34e
yao qiu <yuri1100@qq.com>
2025-12-24 12:24:24 +0800

    add a bash script as an example to show how to run i2v in w4a16

---
+++ run_i2v_w4a16.sh
@@ -0,0 +1,11 @@
+export USE_SAGEATTN=1
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+python3 generate_w4a16.py  --task i2v-A14B \
+  --size 1280*720 \
+  --quantized_ckpt_dir /data/models/wan/i2v_14B_NVFP4 \
+  --original_ckpt_dir /data/models/wan/i2v_14B_official \
+  --offload_model True \
+  --convert_model_dtype \
+  --image examples/i2v_input.JPG \
+  --prompt "Summer beach vacation style, a white cat wearing sunglasses sits on a surfboard. The fluffy-furred feline gazes directly at the camera with a relaxed expression. Blurred beach scenery forms the background featuring crystal-clear waters, distant green hills, and a blue sky dotted with white clouds. The cat assumes a naturally relaxed posture, as if savoring the sea breeze and warm sunlight. A close-up shot highlights the feline's intricate details and the refreshing atmosphere of the seaside."
+
